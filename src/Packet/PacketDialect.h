60e610565512994f6c2d7b2f6e7bd55abbe734b8
bzhanggg <blz19@scarletmail.rutgers.edu>
2025-02-27 19:24:04 -0500

    initial commit

---
+++ src/Packet/PacketDialect.h
@@ -0,0 +1,9 @@
+#ifndef DIALECT_PACKETDIALECT_H_
+#define DIALECT_PACKETDIALECT_H_
+
+#include "mlir/IR/DialectImplementation.h"
+
+// include the generated source file from TableGen
+#include "src/Dialect/PacketDialect.h.inc"
+
+#endif // DIALECT_PACKETDIALECT_H_
