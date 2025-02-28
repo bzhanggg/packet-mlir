7a20f9a4bec8f334b27f9d207271dd4756fb87bc
bzhanggg <blz19@scarletmail.rutgers.edu>
2025-02-27 19:34:13 -0500

    add necessary includes for PacketDialect.td to make ninja-build work

---
+++ src/Packet/PacketTypes.h
@@ -0,0 +1,11 @@
+#ifndef DIALECT_PACKET_PACKTTYPES_H_
+#define DIALECT_PACKET_PACKTTYPES_H_
+
+// Required because the .h.inc file refers to MLIR classes and does not itself
+// have any includes.
+#include "mlir/IR/DialectImplementation.h"
+
+#define GET_TYPEDEF_CLASSES
+#include "lib/Dialect/Poly/PolyTypes.h.inc"
+
+#endif  // DIALECT_PACKET_PACKTTYPES_H_
