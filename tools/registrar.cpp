463f58474fa5888024f5e077583a0e0c460888cf
bzhanggg <blz19@scarletmail.rutgers.edu>
2025-03-07 20:21:22 -0500

    add registrar tool

---
+++ tools/registrar.cpp
@@ -0,0 +1,16 @@
+#include "src/Packet/PacketDialect.h"
+#include "mlir/Tools/mlir-opt/MlirOptMain.h"
+
+#include "mlir/InitAllDialects.h"
+#include "mlir/InitAllPasses.h"
+
+int main(int argc, char **argv) {
+    mlir::DialectRegistry registry;
+    registry.insert<mlir::packet::PacketDialect>();
+    mlir::registerAllDialects(registry);
+    mlir::registerAllPasses();
+
+    return mlir::asMainReturnCode(
+        mlir::MlirOptMain(argc, argv, "Pass Driver", registry)
+    );
+}
