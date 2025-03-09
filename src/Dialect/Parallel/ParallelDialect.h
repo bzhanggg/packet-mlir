8b71066fec3470fb4267a1ebe10b4e7b4d5ff674
bzhanggg <blz19@scarletmail.rutgers.edu>
2025-03-08 19:48:36 -0500

    create remaining Parallel dialect files

---
+++ src/Dialect/Parallel/ParallelDialect.h
@@ -0,0 +1,8 @@
+#ifndef DIALECT_PARALLELDIALECT_H_
+#define DIALECT_PARALLELDIALECT_H_
+
+#include "mlir/IR/DialectImplementation.h"
+
+#include "src/Dialect/Parallel/ParallelDialect.h.inc"
+
+#endif // DIALECT_PARALLELDIALECT_H_
