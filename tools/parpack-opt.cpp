7203bd7e5cb4d38e616b40dd38b09d06ae55dbb4
bzhanggg <blz19@scarletmail.rutgers.edu>
2025-04-28 21:45:00 -0400

    begin work on packet dialect

--- tools/parpack-opt.cpp
+++ tools/parpack-opt.cpp
@@ -10,14 +10,14 @@
 
 int main(int argc, char **argv) {
     mlir::DialectRegistry registry;
-    // registry.insert<mlir::parpack::packet::PacketDialect>();
-    registry.insert<mlir::parpack::parallel::ParallelDialect>();
+    registry.insert<mlir::parpack::packet::PacketDialect>();
+    // registry.insert<mlir::parpack::parallel::ParallelDialect>();
     mlir::registerAllDialects(registry);
 
-    mlir::MLIRContext context(registry);
+    // mlir::MLIRContext context(registry);
 
-    mlir::registerCanonicalizerPass();
-    mlir::registerPassManagerCLOptions();
+    // mlir::registerCanonicalizerPass();
+    // mlir::registerPassManagerCLOptions();
 
     return mlir::asMainReturnCode(
         mlir::MlirOptMain(argc, argv, "Parpack Pass Driver", registry)
